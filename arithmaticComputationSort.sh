f7a4261c06dba3693d0f50a13f9ff8d95dc9e14d
rageshwari <ragu.bodke@gmail.com>
2020-03-23 15:15:53 +0530

    First Computation

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -5,3 +5,6 @@
 read -p "Enter first number: " Number1
 read -p "Enter second number: " Number2
 read -p "Enter third number: " Number3
+
+#Computations
+result1=`echo "scale=2 ; $Number1+$Number2*$Number3" | bc -l`
