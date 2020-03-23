ad665a9273677f2927455b4519017feaaa91284e
rageshwari <ragu.bodke@gmail.com>
2020-03-23 15:12:34 +0530

    Taking three inputs from User

--- arithmaticComputationSort.sh
+++ arithmaticComputationSort.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -x
 #Program to computes different arithmatic expressions and sort the result
+
+#Take input from user
+read -p "Enter first number: " Number1
+read -p "Enter second number: " Number2
+read -p "Enter third number: " Number3
